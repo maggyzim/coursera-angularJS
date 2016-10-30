bcb1d9f2c843b1f287b562c984abe6790c146d41
Michael Magondo <Michael Magondo>
2016-10-30 20:04:31 +0200

    final changes

---
+++ assignment1/app.js
@@ -0,0 +1,38 @@
+(function(){
+    'use strict';
+
+    angular.module('myFirtApp', [])
+
+    .controller('myFirstController', function ($scope){
+        $scope.lmsg = "Click to assess items captured.";
+        $scope.lmenu = "";
+        
+   $scope.check = function check(){
+
+        if ($scope.lmenu==""){
+            $scope.lmsg ="Please Enter Data First"
+        } else {
+         
+        var item_array = $scope.lmenu.split(",");
+
+        removeItem(item_array, '');
+    
+        if(item_array.length > 3){
+            $scope.lmsg = "Too Much"
+        } else {
+            $scope.lmsg = "Enjoy"
+        }
+    }
+   }
+   })
+
+   function removeItem(array, item){
+    for(var i in array){
+        if(array[i]==item){
+            array.splice(i,1);
+            break;
+            }
+    }
+   }
+
+})();
